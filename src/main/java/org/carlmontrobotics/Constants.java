79699c77826306e05b8e534a86cd8024eb825ec9
DriverStationComputer <35879629+DriverStationComputer@users.noreply.github.com>
2025-02-25 16:59:56 -0800

    added dt stuff

--- src/main/java/org/carlmontrobotics/Constants.java
+++ src/main/java/org/carlmontrobotics/Constants.java
@@ -1,7 +1,15 @@
 package org.carlmontrobotics;
 
+import org.carlmontrobotics.lib199.swerve.SwerveConfig;
+
+import com.pathplanner.lib.path.PathConstraints;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController.Axis;
 import edu.wpi.first.wpilibj.XboxController.Button;
+
+import static org.carlmontrobotics.Config.CONFIG;
 
 public final class Constants {
     // public static final class Drivetrain {
@@ -20,7 +28,7 @@ public final class Constants {
 
     public static final class OI {
         public static final double MIN_AXIS_TRIGGER_VALUE = 0.2;
-
+		public static final double JOY_THRESH = 0.01;
         public static final class Driver {
             public static final int driverPort = 0;
             public static final int effectorMotorID = 4;
@@ -59,6 +67,192 @@ public final class Constants {
         public static double outtakePincherRPM = 2100;  
 
 	}
+
+    public static final class Drivetrainc {
+	// #region Subsystem Constants
+	public static final double wheelBase = 24.75; //CONFIG.isSwimShady() ? Units.inchesToMeters(19.75) : Units.inchesToMeters(16.75);
+	public static final double trackWidth = 24.75;//CONFIG.isSwimShady() ? Units.inchesToMeters(28.75) : Units.inchesToMeters(23.75);
+	// "swerveRadius" is the distance from the center of the robot to one of the
+	// modules
+	public static final double swerveRadius = Math.sqrt(Math.pow(wheelBase / 2, 2) + Math.pow(trackWidth / 2, 2));
+	// The gearing reduction from the drive motor controller to the wheels
+	// Gearing for the Swerve Modules is 6.75 : 1e
+	public static final double driveGearing = 6.75;
+	// Turn motor shaft to "module shaft"
+	public static final double turnGearing = 150.0 / 7;
+
+	public static final double driveModifier = 1;
+	public static final double wheelDiameterMeters = Units.inchesToMeters(4.0) * 7.36 / 7.65 /*
+																					 * empirical
+																					 * correction
+																					 */;
+	public static final double mu = 1; /* 70/83.2; */
+
+	public static final double NEOFreeSpeed = 5676 * (2 * Math.PI) / 60; // radians/s
+	// Angular speed to translational speed --> v = omega * r / gearing
+	public static final double maxSpeed = NEOFreeSpeed * (wheelDiameterMeters / 2.0) / driveGearing;
+	public static final double maxForward = maxSpeed; // todo: use smart dashboard to figure this out
+	public static final double maxStrafe = maxSpeed; // todo: use smart dashboard to figure this out
+	// seconds it takes to go from 0 to 12 volts(aka MAX)
+	public static final double secsPer12Volts = 0.1;
+
+	// maxRCW is the angular velocity of the robot.
+	// Calculated by looking at one of the motors and treating it as a point mass
+	// moving around in a circle.
+	// Tangential speed of this point mass is maxSpeed and the radius of the circle
+	// is sqrt((wheelBase/2)^2 + (trackWidth/2)^2)
+	// Angular velocity = Tangential speed / radius
+	public static final double maxRCW = maxSpeed / swerveRadius;
+
+	public static final boolean[] reversed = { false, false, false, false };
+	// public static final boolean[] reversed = {true, true, true, true};
+	// Determine correct turnZero constants (FL, FR, BL, BR)
+	public static final double[] turnZeroDeg = RobotBase.isSimulation() ? new double[] {-90.0, -90.0, -90.0, -90.0 }
+	: (CONFIG.isSwimShady() ? new double[] { 85.7812, 85.0782, -96.9433, -162.9492 }
+		: new double[] { 17.2266, -96.8555, -95.8008, 85.166 });/* real values here */
+
+	// kP, kI, and kD constants for turn motor controllers in the order of
+	// front-left, front-right, back-left, back-right.
+	// Determine correct turn PID constants
+	public static final double[] turnkP = {12, 12, 23, 23};//sysid for fr that didnt't work{0.099412, 0.13414, 3.6809, 3.6809} //{49, 23,33, 28};//{51.078, 25, 35.946, 30.986}; // {0.00374, 0.00374, 0.00374,
+																	// 0.00374};
+	public static final double[] turnkI = {0, 0, 0, 0};//{ 0, 0.1, 0, 0 };
+	public static final double[] turnkD = {1, 1.55, 0, 2};//{ 0.2/* dont edit */, 0.3, 0.5, 0.4}; // todo: use d
+	// public static final double[] turnkS = {0.2, 0.2, 0.2, 0.2};
+	public static final double[] turnkS = {0.050634, 0.033065, 0.018117, 0.021337};//sysid for fr that didnt't work{0.041796, 0.09111, 0.64804, 1.0873}//{ 0.13027, 0.17026, 0.2, 0.23262 };
+
+	// V = kS + kV * v + kA * a
+	// 12 = 0.2 + 0.00463 * v
+	// v = (12 - 0.2) / 0.00463 = 2548.596 degrees/s
+	public static final double[] turnkV = {2.6153, 2.5924, 2.6495, 2.6705};//sysid for fr that didnt't work{2.6403, 2.6603, 2.6168, 2.5002} //{2.6532, 2.7597, 2.7445, 2.7698};
+	public static final double[] turnkA = {0.18525, 0.13879, 0.23625, 0.25589};//sysid for fr that didnt't work{0.33266, 0.25535, 0.17924, 0.17924} //{ 0.17924, 0.17924, 0.17924, 0.17924 };
+
+	// kP is an average of the forward and backward kP values
+	// Forward: 1.72, 1.71, 1.92, 1.94
+	// Backward: 1.92, 1.92, 2.11, 1.89
+	// Order of modules: (FL, FR, BL, BR)
+	public static final double[] drivekP = CONFIG.isSwimShady() ? new double[] { 2.8, 2.8, 2.8, 2.8 }
+	: new double[] {1.75, 1.75, 1.75, 1.75}; //{2.2319, 2.2462, 2.4136, 3.6862}; // {1.82/100, 1.815/100, 2.015/100,
+															// 1.915/100};
+	public static final double[] drivekI = { 0.1, 0.1, 0.1, 0.1 };
+	public static final double[] drivekD = { 0, 0, 0, 0 };
+	public static final boolean[] driveInversion = (CONFIG.isSwimShady()
+	? new boolean[] { false, false, false, false }
+	: new boolean[] { false, true, false, true });
+	public static final boolean[] turnInversion = { true, true, true, true };
+	// kS
+	// public static final double[] kForwardVolts = { 0.26744, 0.31897, 0.27967, 0.2461 };
+    public static final double[] kForwardVolts = {0, 0, 0, 0}; //{0.59395, 0.52681, 0.11097, 0.17914};      //{ 0.2, 0.2, 0.2, 0.2 };
+	public static final double[] kBackwardVolts = kForwardVolts;
+
+	//kV
+	// public static final double[] kForwardVels = { 2.81, 2.9098, 2.8378, 2.7391 };
+    public static final double[] kForwardVels = { 0, 0, 0, 0 };//{2.4114, 2.7465, 2.7546, 2.7412};        //{ 0, 0, 0, 0 };//volts per m/s
+	public static final double[] kBackwardVels = kForwardVels;
+
+	//kA
+	// public static final double[] kForwardAccels = { 1.1047 / 2, 0.79422 / 2, 0.77114 / 2, 1.1003 / 2 };
+    public static final double[] kForwardAccels = { 0, 0, 0, 0 };//{0.31958, 0.33557, 0.70264, 0.46644};    //{ 0, 0, 0, 0 };// volts per m/s^2
+	public static final double[] kBackwardAccels = kForwardAccels;
+
+	public static final double autoMaxSpeedMps = 0.35 * 4.4; // Meters / second
+	public static final double autoMaxAccelMps2 = mu * 9.81; // Meters / seconds^2
+	public static final double autoMaxVolt = 10.0; // For Drivetrain voltage constraint in RobotPath.java
+	// The maximum acceleration the robot can achieve is equal to the coefficient of
+	// static friction times the gravitational acceleration
+	// a = mu * 9.8 m/s^2
+	public static final double autoCentripetalAccel = mu * 9.81 * 2;
+
+	public static final boolean isGyroReversed = true;
+
+	// PID values are listed in the order kP, kI, and kD
+	public static final double[] xPIDController = CONFIG.isSwimShady() ? new double[] { 4, 0.0, 0.0 }
+	: new double[] { 2, 0.0, 0.0 };
+	public static final double[] yPIDController = xPIDController;
+	public static final double[] thetaPIDController = CONFIG.isSwimShady() ? new double[] { 0.10, 0.0, 0.001 }
+	: new double[] {0.05, 0.0, 0.00};
+
+	public static final SwerveConfig swerveConfig = new SwerveConfig(wheelDiameterMeters, driveGearing, mu,
+	autoCentripetalAccel, kForwardVolts, kForwardVels, kForwardAccels, kBackwardVolts, kBackwardVels,
+	kBackwardAccels, drivekP, drivekI, drivekD, turnkP, turnkI, turnkD, turnkS, turnkV, turnkA, turnZeroDeg,
+	driveInversion, reversed, driveModifier, turnInversion);
+
+	// public static final Limelight.Transform limelightTransformForPoseEstimation =
+	// Transform.BOTPOSE_WPIBLUE;
+
+	// #endregion
+
+	// #region Ports
+	//I think all of these are right
+	public static final int driveFrontLeftPort = 11;
+	public static final int driveFrontRightPort = 12;
+	public static final int driveBackLeftPort = 13;
+	public static final int driveBackRightPort = 14;
+
+	public static final int turnFrontLeftPort = 1;
+	public static final int turnFrontRightPort = 2;
+	public static final int turnBackLeftPort = 3;
+	public static final int turnBackRightPort = 4;
+	//to be configured
+	public static final int canCoderPortFL = 1; //0
+	public static final int canCoderPortFR = 2;
+	public static final int canCoderPortBL = 3;
+	public static final int canCoderPortBR = 0; //1
+
+	// #endregion
+
+	// #region Command Constants
+
+	public static double kNormalDriveSpeed = 1; // Percent Multiplier	
+	public static double kNormalDriveRotation = 0.5; // Percent Multiplier
+	public static double kSlowDriveSpeed = 0.4; // Percent Multiplier
+	public static double kSlowDriveRotation = 0.250; // Percent Multiplier
+
+	//baby speed values, i just guessed the percent multiplier. TODO: find actual ones we wana use
+	public static double kBabyDriveSpeed = 0.3;
+	public static double kBabyDriveRotation = 0.2;
+	public static double kAlignMultiplier = 1D / 3D;
+	public static final double kAlignForward = 0.6;
+
+	public static final double wheelTurnDriveSpeed = 0.0001; // Meters / Second ; A non-zero speed just used to
+													// orient the wheels to the correct angle. This
+													// should be very small to avoid actually moving the
+													// robot.
+
+	public static final double[] positionTolerance = { Units.inchesToMeters(.5), Units.inchesToMeters(.5), 5 }; // Meters,
+																									// Meters,
+																									// Degrees
+	public static final double[] velocityTolerance = { Units.inchesToMeters(1), Units.inchesToMeters(1), 5 }; // Meters,
+																												// Meters,
+																												// Degrees/Second
+
+	// #endregion
+	// #region Subsystem Constants
+
+	// "swerveRadius" is the distance from the center of the robot to one of the
+	// modules
+	public static final double turnkP_avg = (turnkP[0] + turnkP[1] + turnkP[2] + turnkP[3]) / 4;
+	public static final double turnIzone = .1;
+
+	public static final double driveIzone = .1;
+
+	public static final class Autoc {
+		// public static final RobotConfig robotConfig = new RobotConfig( /*
+		// 																	 * put in
+		// 																	 * Constants.Drivetrain.Auto
+		// 																	 */
+		// false, // replan at start of path if robot not at start of path?
+		// false, // replan if total error surpasses total error/spike threshold?
+		// 1.5, // total error threshold in meters that will cause the path to be replanned
+		// 0.8 // error spike threshold, in meters, that will cause the path to be replanned
+		// );
+		public static final PathConstraints pathConstraints = new PathConstraints(1.54, 6.86, 2 * Math.PI,
+		2 * Math.PI); // The constraints for this path. If using a differential drivetrain, the
+					// angular constraints have no effect.
+		}
+	}		
+
+
     public static final class CoralEffectorc {
 		
         public static final double kP = 0.0;
