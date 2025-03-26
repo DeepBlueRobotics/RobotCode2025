551e5fad80a6fb41c080814ead24c45e6d395ffc
Brandon <brandonshen123@gmail.com>
2025-03-25 17:42:27 -0700

    fixed slowmode (untested)

--- src/main/java/org/carlmontrobotics/commands/TeleopDrive.java
+++ src/main/java/org/carlmontrobotics/commands/TeleopDrive.java
@@ -82,15 +82,16 @@ public class TeleopDrive extends Command {
     // SmartDashboard.putNumber("strafeIN", strafe);
     // SmartDashboard.putNumber("turnIN", rotateClockwise);
     System.out.println("fwd str rcw: "+forward+", "+strafe+", "+rotateClockwise);
-    if (Math.abs(forward) <= Constants.OI.JOY_THRESH)
+    boolean slow2 = slow.getAsBoolean();
+    if (Math.abs(forward) <= Constants.OI.JOY_THRESH && !slow2)
       forward = 0;
     else
       forward *= maxForward;
-    if (Math.abs(strafe) <= Constants.OI.JOY_THRESH)
+    if (Math.abs(strafe) <= Constants.OI.JOY_THRESH && !slow2)
       strafe = 0.0;
     else
       strafe *= maxStrafe;
-    if (Math.abs(rotateClockwise) <= Constants.OI.JOY_THRESH)
+    if (Math.abs(rotateClockwise) <= Constants.OI.JOY_THRESH && !slow2)
       rotateClockwise = 0.0;
     else
       rotateClockwise *= maxRCW;
