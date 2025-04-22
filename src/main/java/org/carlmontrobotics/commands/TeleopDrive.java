969352fe186267972eeb0b18e728e36fa7453c41
Tim <73599525+timtogan@users.noreply.github.com>
2025-04-21 21:42:20 -0700

    made baby mode on by default (revert this after Celebrate the Music)

--- src/main/java/org/carlmontrobotics/commands/TeleopDrive.java
+++ src/main/java/org/carlmontrobotics/commands/TeleopDrive.java
@@ -59,7 +59,7 @@ public class TeleopDrive extends Command {
     double[] speeds = getRequestedSpeeds();
     // SmartDashboard.putNumber("Elapsed time", currentTime - prevTimestamp);
     prevTimestamp = currentTime;
-    babyMode = SmartDashboard.getBoolean("babymode", false);
+    babyMode = SmartDashboard.getBoolean("babymode", true);
     // kSlowDriveRotation = SmartDashboard.getNumber("slow turn const", kSlowDriveRotation);
     // kSlowDriveSpeed = SmartDashboard.getNumber("slow speed const", kSlowDriveSpeed);
     // kNormalDriveRotation = SmartDashboard.getNumber("normal turn const", kNormalDriveRotation);
