2b5a326199668239b75e8765b5b527789c732c31
Tim <73599525+timtogan@users.noreply.github.com>
2025-05-15 10:10:33 -0700

    made a seperate toggle for L4 in babyMode

--- src/main/java/org/carlmontrobotics/commands/ElevatorToPos.java
+++ src/main/java/org/carlmontrobotics/commands/ElevatorToPos.java
@@ -8,10 +8,12 @@ import com.playingwithfusion.TimeOfFlight;
 import static org.carlmontrobotics.commands.TeleopDrive.babyMode;
 import static org.carlmontrobotics.Constants.CoralEffectorc.*;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ElevatorToPos extends Command {
     private Elevator elevator;
     private double pos;
+    private boolean enableL4;
     private TimeOfFlight distanceSensor = new TimeOfFlight(CORAL_DISTANCE_SENSOR_PORT);
     public ElevatorToPos(Elevator elevator, double pos) {
         this.elevator = elevator;
@@ -20,11 +22,12 @@ public class ElevatorToPos extends Command {
     }
     @Override
     public void initialize() {
-
+        SmartDashboard.putBoolean("Enable L4", false);
     }
     @Override
     public void execute() {
-        if (!babyMode || pos < 1){
+    enableL4 = SmartDashboard.getBoolean("Enable L4", false);
+        if (!babyMode || enableL4 || pos < 1){
             if (babyMode) { if (distanceSensor.getRange() > CORAL_DISTANCE_SENSOR_DISTANCE) {
                 elevator.setGoal(pos); }
                 else {elevator.stopElevator();}
