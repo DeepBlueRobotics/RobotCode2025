4439c62d1b2be35179fe01bb398e9ed21bece00a
Tim <73599525+timtogan@users.noreply.github.com>
2025-06-05 21:39:02 -0700

    organized coral constants

--- src/main/java/org/carlmontrobotics/Constants.java
+++ src/main/java/org/carlmontrobotics/Constants.java
@@ -452,16 +452,20 @@ public final class Constants {
         public final static int CORAL_DISTANCE_SENSOR_PORT = 6;
 
         public final static int CORAL_DISTANCE_SENSOR_DISTANCE = 150; //mm
+		public final static double CORAL_EFFECTOR_DISTANCE_SENSOR_OFFSET = 0.2 ; 
+        public final static double KP = 0.1; 
+        public final static double KI = 0;
+        public final static double KD = 0;
+
+
         public final static double CORAL_INTAKE_ERR = .1;//encoder units - rotations
+
         public final static double INPUT_FAST_SPEED =0.07; 
 		public final static double INTAKE_BACKWARDS_SPEED = -0.06; 
         public final static double INPUT_SLOW_SPEED = 0.04; 
         public final static double OUTPUT_SPEED = 0.4; 
 		public final static double FAST_OUTPUT_SPEED = 0.6;
-        public final static double CORAL_EFFECTOR_DISTANCE_SENSOR_OFFSET = 0.2 ; 
-        public final static double KP = 0.1; 
-        public final static double KI = 0;
-        public final static double KD = 0;
+
         public final static double INTAKE_TIME_OUT = 0.5;
         public final static double OUTTAKE_TIME_OUT = 10;
         public final static double MANUAL_INTAKE_TIME_OUT = 1;
