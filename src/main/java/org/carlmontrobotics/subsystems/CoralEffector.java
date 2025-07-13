2aa35d398b5dfc9aee174d8c895040a6f6c6f1d5
Tim <73599525+timtogan@users.noreply.github.com>
2025-07-12 20:19:40 -0700

    removed an unused import that was stopping the code from building

--- src/main/java/org/carlmontrobotics/subsystems/CoralEffector.java
+++ src/main/java/org/carlmontrobotics/subsystems/CoralEffector.java
@@ -23,7 +23,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import net.bytebuddy.build.Plugin;
 
 import static org.carlmontrobotics.Constants.*;
 
