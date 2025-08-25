61a5c95500d839fa78d069947c853d172216bbb6
Tim <73599525+timtogan@users.noreply.github.com>
2025-08-25 12:05:33 -0700

    added photonlib and made a photonVision subsystem

---
+++ src/main/java/org/carlmontrobotics/subsystems/PhotonVision.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.carlmontrobotics.subsystems;
+
+import java.util.List;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import java.util.Optional;
+
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.targeting.proto.PhotonPipelineResultProto;
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.proto.Photon;
+import org.photonvision.PhotonUtils;
+
+public class PhotonVision extends SubsystemBase {
+  /** Creates a new PhotonVision. */
+  //name of the camera in the PhotonVision UI
+  private PhotonCamera camera = new PhotonCamera("DriverCam"); 
+  //layout of the field
+  private final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField); 
+  //transation from the center of the robot to the camera position
+  //FIXME ask design what is the translation
+  private final Transform3d kRobotToCam = new Transform3d(new Translation3d(0.0, 0.0, 0.0), new Rotation3d(0, 0, 0)); 
+  //transformation from the camera to the center of the robot
+  private final Transform3d kCamToRobot = kRobotToCam.inverse();
+  /**
+    * estimates the robot's pose on the field using the apriltags
+    * uses MULTI_TAG_PNP_ON_COPROCESSOR which takes in account all the apriltags that the camera can sees and uses all of them to estiamte the position 
+    * change it to LOWEST_AMBIGUITY if pi is too slow (I think it should be fine though)
+   */
+  private PhotonPoseEstimator photonEstimator = new PhotonPoseEstimator(kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, kRobotToCam);
+  public PhotonVision() {
+    List<PhotonPipelineResult> latestResult;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
